d11df754456af54bb76ce804633cc038ce3f2924
Christopher Mora <cmora09@gmail.com>
2016-01-26 14:19:05 -0500

    Initial Commit

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("play-button").onclick = function(){
+	document.getElementById("song").play();
+}
+document.getElementById("pause-button").onclick = function(){
+	document.getElementById("song").pause();
+}
